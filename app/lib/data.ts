568ee5147e9f471e4fcead29d50ce526c718f4e2
muhammad_ibrahim <ibrahim@jfreaks.com>
2025-05-09 13:01:17 +0500

    add:Suspense in the dashboard home page

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,8 +16,8 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 10000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
@@ -32,6 +32,8 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    console.log('Fetching latest inovices...');
+    await new Promise((resolve) => setTimeout(resolve, 5000));
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
