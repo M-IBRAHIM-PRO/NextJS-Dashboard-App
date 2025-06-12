18198f61808d3fac6854bace7d0cd75d49967448
muhammad_ibrahim <ibrahim@jfreaks.com>
2025-06-12 19:48:13 +0500

    fix:changed the url in auth.config

--- auth.config.ts
+++ auth.config.ts
@@ -1,26 +1,27 @@
 import type { NextAuthConfig } from 'next-auth';
- 
+
 import type { Provider } from 'next-auth/providers';
 
 export const authConfig = {
-  providers: [
-    // Add your authentication providers here, for example:
-    // GithubProvider({ clientId: process.env.GITHUB_ID!, clientSecret: process.env.GITHUB_SECRET! }),
-  ] as Provider[],
-  pages: {
-    signIn: '/login',
-  },
-  callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl.origin));
-      }
-      return true;
+    providers: [
+        // Add your authentication providers here, for example:
+        // GithubProvider({ clientId: process.env.GITHUB_ID!, clientSecret: process.env.GITHUB_SECRET! }),
+    ] as Provider[],
+    pages: {
+        signIn: '/login',
     },
-  },
+    callbacks: {
+        authorized({ auth, request: { nextUrl } }) {
+            const baseUrl = process.env.NEXTAUTH_URL || nextUrl.origin;
+            const isLoggedIn = !!auth?.user;
+            const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+            if (isOnDashboard) {
+                if (isLoggedIn) return true;
+                return false; // Redirect unauthenticated users to login page
+            } else if (isLoggedIn) {
+                return Response.redirect(new URL('/dashboard',));
+            }
+            return true;
+        },
+    },
 } satisfies NextAuthConfig;
